4c4f993247d183f14537c270d835eff293d7cd3e
Yael Brown <yaelrbrown@gmail.com>
2019-12-21 20:01:26 -0600

    event handling with methods.

--- reactjssandbox/src/App.js
+++ reactjssandbox/src/App.js
@@ -10,13 +10,20 @@ export default class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    console.log('Was clicked');
+  }
+
   render() {
     return (
       <div className="App">
         {/* <Person name="Yael" age="32"/>
         <Person name="Cookie" age="31"/>
         <Person name="Donut" age="30"/> */}
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
