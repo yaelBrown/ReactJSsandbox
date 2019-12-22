5b6388897b6528683083d53b941e6b472941f3da
Yael Brown <yaelrbrown@gmail.com>
2019-12-21 18:44:37 -0600

    starting props. Created example functional component named Person

---
+++ reactjssandbox/src/components/Person.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Person() {
+  return (
+    <div>
+      <p>I am a person and I am {Math.floor(Math.random() * 30)} years old!</p>
+    </div>
+  )
+}
