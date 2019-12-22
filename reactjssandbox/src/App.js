580e3d1ca4af9e82f765bf12eb14129c5def0458
Yael Brown <yaelrbrown@gmail.com>
2019-12-21 19:38:49 -0600

    Rendered components with state.

--- reactjssandbox/src/App.js
+++ reactjssandbox/src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './components/Person';
 
+export default class App extends Component {
+  state = {
+    persons: [
+      { name: 'Yael', age: 32 },
+      { name: 'Cookie', age: 31 },
+      { name: 'Donut', age: 30 }
+    ]
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <Person/>
-      <Person/>
-      <Person/>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        {/* <Person name="Yael" age="32"/>
+        <Person name="Cookie" age="31"/>
+        <Person name="Donut" age="30"/> */}
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+      </div>
+    );
+  }
 }
-
-export default App;
--- reactjssandbox/src/components/Person.js
+++ reactjssandbox/src/components/Person.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function Person() {
+export default function Person(props) {
   return (
     <div>
-      <p>I am a person and I am {Math.floor(Math.random() * 30)} years old!</p>
+      {/* <p>I am a person and I am {Math.floor(Math.random() * 30)} years old!</p> */}
+      <p>I am {props.name} and I am {props.age} years old!</p>
     </div>
   )
 }
