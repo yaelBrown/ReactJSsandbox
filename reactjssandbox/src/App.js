4d34e26838d7676b4659ba9d30771b102bfab05b
Yael Brown <yaelrbrown@gmail.com>
2019-12-23 18:50:49 -0600

    Finished component lifecycle. Copied sample code.

--- reactjssandbox/src/App.js
+++ reactjssandbox/src/App.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import Person from './components/Person';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    console.log('app.js constuctor');
+    this.setState();
+  }
+
   state = {
     persons: [
       { name: 'Yael', age: 32 },
       { name: 'Cookie', age: 31 },
       { name: 'Donut', age: 30 }
     ]
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFormProps', props);
+    return state;
   }
 
   addToAge = (age) => age++;
@@ -18,6 +29,7 @@ export default class App extends Component {
 
   render() {
     return (
+      console.log('app.js render');
       <div className="App">
         {/* <Person name="Yael" age="32"/>
         <Person name="Cookie" age="31"/>
